eef5d4d4f2027089dc1a8af1ab2d813aae48ba98
kaveer <kaveer.rajcoomar@sdworx.com>
2024-06-28 22:42:12 +0400

    Final commit

--- src/app/add-employee/add-employee.component.ts
+++ src/app/add-employee/add-employee.component.ts
@@ -1,12 +1,36 @@
-import { Component } from '@angular/core';
+import {Component, EventEmitter, Input, Output} from '@angular/core';
+import {FormsModule} from "@angular/forms";
+import {Employee} from "../employee-list/employee-list.component";
 
 @Component({
   selector: 'ems-add-employee',
   standalone: true,
-  imports: [],
+  imports: [
+    FormsModule
+  ],
   templateUrl: './add-employee.component.html',
   styleUrl: './add-employee.component.sass'
 })
 export class AddEmployeeComponent {
+
+  @Output() addEmployeeOutput: EventEmitter<Employee> = new EventEmitter<Employee>();
+
+  name: string = "";
+  position: string = "";
+  experience: number = 0;
+  department: string = "";
+
+  addEmployee() {
+    let emp: Employee|undefined = {
+      id : 0,
+      name : this.name,
+      position : this.position,
+      experience : Number(this.experience) ,
+      department: this.department
+    }
+
+    this.addEmployeeOutput.emit(emp);
+  }
+
 
 }
--- src/app/employee-list/employee-list.component.ts
+++ src/app/employee-list/employee-list.component.ts
@@ -1,12 +1,56 @@
 import { Component } from '@angular/core';
+import {NgClass, NgForOf, NgIf} from "@angular/common";
+import {EmployeeDetailComponent} from "../employee-detail/employee-detail.component";
+import {AddEmployeeComponent} from "../add-employee/add-employee.component";
+
+export interface Employee {
+  id: number;
+  name: string;
+  position: string;
+  experience: number;
+  department: string;
+}
 
 @Component({
   selector: 'ems-employee-list',
   standalone: true,
-  imports: [],
+  imports: [
+    NgForOf,
+    NgClass,
+    EmployeeDetailComponent,
+    NgIf,
+    AddEmployeeComponent
+  ],
   templateUrl: './employee-list.component.html',
   styleUrl: './employee-list.component.sass'
 })
 export class EmployeeListComponent {
 
+  showAddEmployee: boolean = false;
+  employeeDetails : Employee | undefined;
+  employees: Employee[] = [
+    {id : 1, name : "Lucas Rivera", department : "Software Development", experience : 2, position : "Junior"},
+    {id : 2, name : "Emily Watson", department : "Quality Assurance", experience : 5, position : "Senior"},
+    {id : 3, name : "Ethan Cooper", department : "Network and Infrastructure", experience : 10, position : "Lead"},
+    {id : 4, name : "Sophie Bennett", department : "Cybersecurity", experience : 3, position : "Junior"},
+    {id : 5, name : "Chloe Mitchell", department : "IT Support", experience : 22, position : "Lead"},
+    {id : 6, name : "Benjamin Flores", department : "Software Development", experience : 12, position : "Lead"},
+  ]
+
+  getEmployeeDetails(emp: Employee) {
+    this.employeeDetails = emp
+    this.showAddEmployee = false;
+  }
+
+  showAddEmployeeComponent() {
+    this.employeeDetails = undefined;
+    this.showAddEmployee = true;
+  }
+
+  getAddedEmployee(emp: Employee): void{
+    const lastEmployee: Employee = this.employees[this.employees.length - 1];
+    emp.id = lastEmployee.id + 1;
+
+    this.employees.push(emp);
+  }
 }
